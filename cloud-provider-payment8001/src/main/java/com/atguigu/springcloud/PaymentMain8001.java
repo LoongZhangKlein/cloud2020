6fd49874d6d056d1e6b768bade0ac66e677a5994
loongzhang <1518736392@qq.com>
2023-01-18 17:56:23 +0800

    基本框架搭建 业务流程搭建

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * @author loongzhang
+ * @Description DOING
+ * @date ${DATE}-${TIME}
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(PaymentMain8001.class, args);
+        ConfigurableEnvironment environment = run.getEnvironment();
+
+        System.out.println("PaymentMain8001启动成功 !"+environment.getProperty("server.port"));
+    }
+}
